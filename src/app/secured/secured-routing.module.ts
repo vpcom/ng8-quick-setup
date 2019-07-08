4eddbaa3c7f116b856a1960c0f1350526dee0b4f
Vincent Perrin <vpcom01@gmail.com>
2019-07-08 23:37:15 +0200

    feat(routing): secured module

--- src/app/secured/secured-routing.module.ts
+++ src/app/secured/secured-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeContainerComponent } from './home-container/home-container.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeContainerComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
